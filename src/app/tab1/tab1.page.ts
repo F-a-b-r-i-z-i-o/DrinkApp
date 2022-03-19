67b8e0b8c60a8f378c4ca791b02bcfbd858e5544
F-A-B-R-I-Z-I-O <fabriziofagiolo18@gmail.com>
2022-03-19 23:42:04 +0100

    start

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  cocktailName = [];
+  coktailList: any;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+  getData() {
+    this.ionChange(event);
+    this.http
+      .get(
+        'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' +
+          this.cocktailName
+      )
+      .subscribe((data: any) => {
+        console.log(data);
+        this.coktailList = data.drinks;
+        console.log(this.coktailList);
+      });
+    this.cocktailName.pop();
+    console.log(this.cocktailName);
+  }
+
+  ionChange(event) {
+    console.log(event.detail.value);
+    let name = event.detail.value;
+    this.cocktailName.push(name);
+    console.log('okkkkkkk');
+    console.log(this.cocktailName);
+  }
+
+  ngOnInit() {
+    this.getData();
+  }
 }
