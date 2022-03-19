83ed2e94ad465cfe48d72360f6549098190eda5e
F-A-B-R-I-Z-I-O <fabriziofagiolo18@gmail.com>
2022-03-20 00:08:17 +0100

    save1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,23 +8,31 @@ import { HttpClient } from '@angular/common/http';
 export class Tab1Page implements OnInit {
   cocktailName = [];
   coktailList: any;
+  control = null;
 
   constructor(private http: HttpClient) {}
 
   getData() {
-    this.ionChange(event);
-    this.http
-      .get(
-        'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' +
-          this.cocktailName
-      )
-      .subscribe((data: any) => {
-        console.log(data);
-        this.coktailList = data.drinks;
-        console.log(this.coktailList);
-      });
-    this.cocktailName.pop();
-    console.log(this.cocktailName);
+    this.control = true;
+    if ((this.control = true)) {
+      this.ionChange(event);
+      this.http
+        .get(
+          'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' +
+            this.cocktailName
+        )
+        .subscribe((data: any) => {
+          console.log(data);
+          this.coktailList = data.drinks;
+          console.log(this.coktailList);
+          this.cocktailName.pop();
+          console.log(this.cocktailName);
+        });
+    } else {
+      this.coktailList = [];
+      this.control = false;
+      console.log('Vuoto');
+    }
   }
 
   ionChange(event) {
