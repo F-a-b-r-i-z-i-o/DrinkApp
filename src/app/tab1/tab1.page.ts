0d9548fd9a0435ff2ecec7b78b7386b60c468941
F-A-B-R-I-Z-I-O <fabriziofagiolo18@gmail.com>
2022-03-29 01:09:09 +0200

    aggiorno2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,49 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page implements OnInit {
-  cocktailName = [];
-  coktailList: any;
-  control = null;
-
-  constructor(private http: HttpClient) {}
-
-  getData() {
-    this.control = true;
-    if ((this.control = true)) {
-      this.ionChange(event);
-      this.http
-        .get(
-          'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' +
-            this.cocktailName
-        )
-        .subscribe((data: any) => {
-          console.log(data);
-          this.coktailList = data.drinks;
-          console.log(this.coktailList);
-          this.cocktailName.pop();
-          console.log(this.cocktailName);
-        });
-    } else {
-      this.coktailList = [];
-      this.control = false;
-      console.log('Vuoto');
-    }
-  }
-
-  ionChange(event) {
-    console.log(event.detail.value);
-    let name = event.detail.value;
-    this.cocktailName.push(name);
-    console.log('okkkkkkk');
-    console.log(this.cocktailName);
-  }
-
-  ngOnInit() {
-    this.getData();
-  }
+  ngOnInit() {}
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  cocktailName = [];
+  ingridients: any;
+  coktailList: any;
+  control = null;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+  getData() {
+    this.control = true;
+    if ((this.control = true)) {
+      this.ionChange(event);
+      this.http
+        .get(
+          'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' +
+            this.cocktailName
+        )
+        .subscribe((data: any) => {
+          console.log(data);
+          this.coktailList = data.drinks;
+          console.log(this.coktailList);
+          this.cocktailName.pop();
+          console.log(this.cocktailName);
+        });
+    } else {
+      this.coktailList = [];
+      this.control = false;
+      console.log('Vuoto');
+    }
+  }
+
+  ionChange(event) {
+    console.log(event.detail.value);
+    let name = event.detail.value;
+    this.cocktailName.push(name);
+    console.log('okkkkkkk');
+    console.log(this.cocktailName);
+  }
+
+  ngOnInit() {
+    this.getData();
+  }
 }
